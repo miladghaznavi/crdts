6d78fc81a472d788bebbc3074116a6d32d945283
miladghaznavi <miladghaznavi@gmail.com>
2020-03-07 12:43:25 -0500

    Adding source file for the implementation of timestamp

---
+++ sb/timestamp.cc
@@ -0,0 +1,41 @@
+#include "timestamp.hh"
+
+void Timestamp::replica_id(uint64_t replica_id) {
+    this->_replica_id = replica_id;
+    this->_uid = replica_id;
+}
+
+bool operator < (const Timestamp &t1, const Timestamp &t2) {
+    // The comparison concatenates sequence numbers with uids to avoid ties.
+    return t1._seq_number < t2._seq_number or
+           (t1._seq_number == t2._seq_number and t1._uid < t2._uid);
+}
+
+bool operator == (const Timestamp& t1, const Timestamp& t2) {
+    return t1._seq_number == t2._seq_number and t1._uid == t2._uid;
+}
+
+std::ostream& operator << (std::ostream &out, const Timestamp& t) {
+    out << t._seq_number << t._uid;
+    return out;
+}
+
+void Timestamp::update() {
+    // TODO: replace replica_id with an external call that returns the replica's identifier.
+    _seq_number ++;
+    _uid = _replica_id;
+}
+
+bool Timestamp::beginning_of_time() {
+    return _seq_number == 0;
+}
+
+uint64_t Timestamp::replica_id() {
+    return _replica_id;
+}
+
+void Timestamp::copy(const Timestamp &t) {
+    this->_uid = t._uid;
+    this->_seq_number = t._seq_number;
+}
+
--- sb/timestamp.hh
+++ sb/timestamp.hh
@@ -1,6 +1,8 @@
 #ifndef CRDTS_TIMESTAMP_HH
 #define CRDTS_TIMESTAMP_HH
 
+#include <ostream>
+
 struct Timestamp {
 private:
     uint64_t _seq_number{}; // A monotonically increasing sequence number.
@@ -10,47 +12,41 @@ private:
 public:
     /// Creates a new timestamp
     /// \param replica_id the replica's unique identifier
-    void replica_id(uint64_t replica_id) {
-        this->_replica_id = replica_id;
-        this->_uid = replica_id;
-    }
+    void replica_id(uint64_t replica_id);
 
     /// Compares the object timestamp with a given timestamp
-    /// \param t the given timestamp
-    /// \return true if the object timestamp ``happens before'' the given timestamp, otherwise false.
-    bool operator < (const Timestamp& t) {
-        // The comparison concatenates sequence numbers with uids to avoid ties.
-        return this->_seq_number < t._seq_number or
-               (this->_seq_number == t._seq_number and this->_uid < t._uid);
-    }
+    /// \param t1 the given timestamp
+    /// \param t2 the given timestamp
+    /// \return true if t1 ``happens before'' t2, otherwise false.
+    friend bool operator < (const Timestamp& t1, const Timestamp& t2);
+
+    /// Compares the equality of two timestamp objects
+    /// \param t1 the first timestamp
+    /// \param t2 the second timestamp
+    /// \return true of the two timestamps are equal, otherwise false
+    friend bool operator == (const Timestamp& t1, const Timestamp& t2);
+
+    /// Feeding output stream with a timestamp
+    /// \param out the output stream
+    /// \param t the timestamp
+    /// \return output stream
+    friend std::ostream& operator << (std::ostream &out, const Timestamp& t);
 
     /// Update the timestamp by incrementing its sequence number
-    void update() {
-        // TODO: handle the overflow of seq_number
-        // TODO: replace replica_id with an external call that returns the replica's identifier.
-        _seq_number ++;
-        _uid = _replica_id;
-    }
+    void update();
 
     /// Checks if the current timestamp is at the beginning of time
     /// \return true if at beginning of time, otherwise false.
-    bool beginning_of_time() {
-        return _seq_number == 0;
-    }
+    bool beginning_of_time();
 
     /// Gets the value of replica
     /// \return
-    uint64_t replica_id() {
-        return _replica_id;
-    }
+    uint64_t replica_id();
 
     /// Copies the sequence number and uid of a given timestamp.
     /// The replica id is not changed.
     /// \param t the given timestamp
-    void copy(const Timestamp &t) {
-        this->_uid = t._uid;
-        this->_seq_number = t._seq_number;
-    }
+    void copy(const Timestamp &t);
 };
 
 #endif //CRDTS_TIMESTAMP_HH
