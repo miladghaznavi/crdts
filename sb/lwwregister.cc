bf3e773890119c4775eaa1c229fca7c4a090e120
miladghaznavi <miladghaznavi@gmail.com>
2020-03-07 12:35:51 -0500

    Move the comments to the header file

--- sb/lwwregister.cc
+++ sb/lwwregister.cc
@@ -1,13 +1,9 @@
 #include "lwwregister.hh"
 
-/// Constructs a LWWRegister
-/// \param replica_id the replica
 LWWRegister::LWWRegister(uint64_t replica_id) {
     _timestamp.replica_id(replica_id);
 }
 
-/// Queries the value of the register
-/// \return the latest value
 std::string LWWRegister::value() {
     if (!initialized()) {
         throw "Querying an uninitialized register";
@@ -16,15 +12,11 @@ std::string LWWRegister::value() {
     return _value;
 }
 
-/// Updates the value of the register with a given value
-/// \param value the given value
 void LWWRegister::assign(const std::string& value) {
     this->_value = value;
     this->_timestamp.update();
 }
 
-/// Updates with a given register if this register is more recent
-/// \param reg the given register
 void LWWRegister::merge(const LWWRegister &reg) {
     if (this->_timestamp < reg._timestamp) {
         this->_value = reg._value;
@@ -32,14 +24,10 @@ void LWWRegister::merge(const LWWRegister &reg) {
     }//if
 }
 
-/// Gets the replica id
-/// \return the replica's id
 uint64_t LWWRegister::replica_id() {
     return this->_timestamp.replica_id();
 }
 
-/// Gets if the register has been initialized
-/// \return true if the register is initialized, otherwise false
 bool LWWRegister::initialized() {
     return !this->_timestamp.beginning_of_time();
 }
--- sb/lwwregister.hh
+++ sb/lwwregister.hh
@@ -11,16 +11,28 @@ private:
     std::string _value;
 
 public:
-    LWWRegister(uint64_t replica_id);
+    /// Constructs a LWWRegister
+    /// \param replica_id the replica
+    explicit LWWRegister(uint64_t replica_id);
 
+    /// Queries the value of the register
+    /// \return the latest value
     std::string value();
 
+    /// Assigns a given value to the register
+    /// \param value the given value
     void assign(const std::string& value);
 
+    /// Merge a given register with the local register
+    /// \param reg the given register
     void merge(const LWWRegister& reg);
 
+    /// Gets the replica id
+    /// \return the replica's id
     uint64_t replica_id();
 
+    /// Gets if the register has been initialized
+    /// \return true if the register is initialized, otherwise false
     bool initialized();
 };
 
