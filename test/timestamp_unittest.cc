7767e2df8272c0ec296e1304fdfb11ac907bd9f7
miladghaznavi <miladghaznavi@gmail.com>
2020-03-07 18:57:00 -0500

    Fix a typo in a file name, and remove the redundant file

--- test/timestamp_unittest.cc
+++ test/timestamp_unittest.cc
@@ -2,45 +2,59 @@
 #include "../sb/timestamp.hh"
 
 namespace {
-    TEST(Timestamp, DefaultConstructor) {
-        #define REPLICA1_ID 1
-        Timestamp t;
-        t.replica_id(REPLICA1_ID);
+    TEST(Timestamp, Intialize) {
+        #define REPLICA_ID 10
+        Timestamp reg;
+        reg.replica_id(REPLICA_ID);
 
-        EXPECT_EQ(REPLICA1_ID, t.replica_id());
-        EXPECT_TRUE(t.beginning_of_time());
+        EXPECT_EQ(reg.replica_id(), REPLICA_ID);
+        EXPECT_TRUE(reg.beginning_of_time());
     }//TEST
 
     TEST(Timestamp, Update) {
-        #define REPLICA1_ID 1
         Timestamp t;
-        t.replica_id(REPLICA1_ID);
-        EXPECT_TRUE(t.beginning_of_time());
+        auto random_repeat = random() % 100 + 1;
+        for (int i = 0; i < random_repeat; ++i)
+            t.update();
 
-        t.update();
-        EXPECT_FALSE(t.beginning_of_time());
-    }
+        EXPECT_EQ(t.sequence_number(), random_repeat);
+    }//TEST
 
     TEST(Timestamp, Copy) {
-        #define REPLICA1_ID 1
-        #define REPLICA2_ID 2
-
         Timestamp t1;
-        t1.replica_id(REPLICA1_ID);
+        t1.replica_id(1);
+        auto random_repeat = random() % 100 + 1;
+        for (int i = 0; i < random_repeat; ++i)
+            t1.update();
 
         Timestamp t2;
-        t2.replica_id(REPLICA2_ID);
-
-        EXPECT_TRUE(t1 < t2);
-        EXPECT_FALSE(t2 < t1);
-
+        t2.replica_id(3);
         t2.copy(t1);
-        EXPECT_FALSE(t1 < t2);
-        EXPECT_FALSE(t2 < t1);
+        EXPECT_TRUE(t1 == t2);
 
         t1.update();
         t2.update();
+        EXPECT_TRUE(t1 != t2);
+    }//TEST
+
+    TEST(Timestamp, Comparison) {
+        #define REPLICA1_ID 1
+        Timestamp t1, t1_copy;
+        t1.replica_id(REPLICA1_ID);
+        t1_copy.replica_id(t1.replica_id());
+        EXPECT_TRUE(t1 == t1_copy);
+        EXPECT_FALSE(t1 != t1_copy);
+
+        #define REPLICA2_ID 2
+        Timestamp t2;
+        t2.replica_id(REPLICA2_ID);
+        EXPECT_FALSE(t1 == t2);
+        EXPECT_TRUE(t1 != t2);
+
+        t1.update();
+        t2.update();
+        EXPECT_FALSE(t1 == t2);
         EXPECT_TRUE(t1 < t2);
-        EXPECT_FALSE(t2 < t1);
-    }
+        EXPECT_TRUE(t1 != t2);
+    }//TEST
 }//namespace
