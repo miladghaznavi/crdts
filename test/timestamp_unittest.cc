8204fd23d198a7a9ed08594b3d22482d19605d10
miladghaznavi <miladghaznavi@gmail.com>
2020-03-09 09:54:50 -0400

    Polish

--- test/timestamp_unittest.cc
+++ test/timestamp_unittest.cc
@@ -14,6 +14,7 @@ namespace {
 
     TEST(Timestamp, Update) {
         Timestamp t;
+        t.replica_id(REPLICA_ID);
         auto random_repeat = random() % TIMESTAMP_TEST_CASES + 1;
         for (int i = 0; i < random_repeat; ++i)
             t.update();
@@ -22,12 +23,14 @@ namespace {
     }//TEST
 
     TEST(Timestamp, Copy) {
+        #define REPLICA1_ID 1
         Timestamp t1;
-        t1.replica_id(1);
+        t1.replica_id(REPLICA1_ID);
         auto random_repeat = random() % TIMESTAMP_TEST_CASES + 1;
         for (int i = 0; i < random_repeat; ++i)
             t1.update();
 
+        #define REPLICA2_ID 2
         Timestamp t2;
         t2.replica_id(3);
         t2.copy(t1);
